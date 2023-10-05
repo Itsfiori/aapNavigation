fb096b215203020cbe7261e1b5455414c2d367d2
pankaj <pankaj.kurade@peolsolutions.com>
2023-10-05 10:35:34 +0530

    510

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,14 +10,15 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("demo.suresh.compconsumer.controller.View1", {
-            onInit: function () {
+            onInit: function () 
+            {
                 var oViewModel = new JSONModel({
                     CustomerID: ""
                 });
                 this.getView().setModel(oViewModel, "local");
             },
 
-            initComponent: function (sCustomerID) { debugger
+            initComponent: function (sCustomerID) {
                 if (!this._customerDetailComponent) {
                     var oCustomerDetailComponent = this.getOwnerComponent().createComponent({
                         usage: "customerDetailComponent",
@@ -32,14 +33,103 @@ sap.ui.define([
                             this._customerDetailComponent = oCustomerDetail;
                         }.bind(this)
                     );
-                } else {
+                }
+                
+                else {
                     this._customerDetailComponent.setCustomerID(sCustomerID);
                 }
             },
+        
 
-            onGo: function (oControlEvent) {
-                let sCustomerID = this.getView().getModel("local").getProperty("/CustomerID");
+            initComponent2: function (sCustomerID) 
+            {
+                if (!this._customerDetailComponent) {
+                    var oCustomerDetailComponent = this.getOwnerComponent().createComponent({
+                        usage: "customerDetailComponent2",
+                        settings: {
+                            customerID: sCustomerID
+                        }
+                    });
+                    oCustomerDetailComponent.then(
+                        function (oCustomerDetail) {
+                            oCustomerDetail.setCustomerID(sCustomerID);
+                            this.byId("customerDetailContainer2").setComponent(oCustomerDetail);
+                            this._customerDetailComponent = oCustomerDetail;
+                        }.bind(this)
+                    );
+                }
+                
+                else {
+                    this._customerDetailComponent.setCustomerID(sCustomerID);
+                }
+            },
+            onGo: function (oControlEvent) { debugger
+                let sCustomerID = 12;
+                //  this.getView().getModel("local").getProperty("/CustomerID");
                 sCustomerID && this.initComponent(sCustomerID);
+
+            },
+
+            onGo2: function (oControlEvent) { debugger
+                let sCustomerID = 12;
+                //  this.getView().getModel("local").getProperty("/CustomerID");
+                sCustomerID && this.initComponent2(sCustomerID);
+            },
+
+            onAfterRendering: function () {
+                var oSplitCont = this.getSplitContObj(),
+                    ref = oSplitCont.getDomRef() && oSplitCont.getDomRef().parentNode;
+                // set all parent elements to 100% height, this should be done by app developer, but just in case
+                if (ref && !ref._sapUI5HeightFixed) {
+                    ref._sapUI5HeightFixed = true;
+                    while (ref && ref !== document.documentElement) {
+                        var $ref = jQuery(ref);
+                        if ($ref.attr("data-sap-ui-root-content")) { // Shell as parent does this already
+                            break;
+                        }
+                        if (!ref.style.height) {
+                            ref.style.height = "100%";
+                        }
+                        ref = ref.parentNode;
+                    }
+                }
+            },
+    
+            onPressNavToDetail: function () {
+                this.getSplitContObj().to(this.createId("detailDetail"));
+            },
+    
+            onPressDetailBack: function () {
+                this.getSplitContObj().backDetail();
+            },
+    
+            onPressMasterBack: function () {
+                this.getSplitContObj().backMaster();
+            },
+    
+            onPressGoToMaster: function () {
+                this.getSplitContObj().toMaster(this.createId("master2"));
+            },
+    
+            onListItemPress: function (oEvent) {
+                var sToPageId = oEvent.getParameter("listItem").getCustomData()[0].getValue();
+    
+                this.getSplitContObj().toDetail(this.createId(sToPageId));
+            },
+    
+            onPressModeBtn: function (oEvent) {
+                var sSplitAppMode = oEvent.getSource().getSelectedButton().getCustomData()[0].getValue();
+    
+                this.getSplitContObj().setMode(sSplitAppMode);
+                MessageToast.show("Split Container mode is changed to: " + sSplitAppMode, { duration: 5000 });
+            },
+    
+            getSplitContObj: function () {
+                var result = this.byId("SplitContDemo");
+                if (!result) {
+                    Log.error("SplitApp object can't be found");
+                }
+                return result;
             }
         });
     });
